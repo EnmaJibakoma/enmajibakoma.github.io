7ad3fa35129261eaae50160cc330ae85f8943c68
vesnea <104858535+seavalanche@users.noreply.github.com>
2026-04-26 00:58:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 // import logo from './logo.svg';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import logo2 from './Assets/caffeinerabbit.bsky.social-3mdjsos6m6s2a.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          aksdjfhgaksdfjghaksdlfjhalskfdjh
-          asdkfjghasldfjhbasdjkfhasdf'
-          askdfaksdhfjg
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://bsky.app/profile/seavalanche.bsky.social"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to my bluesky
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo2} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            aksdjfhgaksdfjghaksdlfjhalskfdjh
+            asdkfjghasldfjhbasdjkfhasdf'
+            askdfaksdhfjg
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <a
+            className="App-link"
+            href="https://bsky.app/profile/seavalanche.bsky.social"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to my bluesky
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
